39d6be77424087fb55fc8bbf024f7eeae70fde41
ccubed.techno <Ccubed.techno@gmail.com>
2014-02-10 16:58:20 -0500

    Organization
    Just changing the names to make them more readable

---
+++ SummerCamp.php
@@ -0,0 +1,15 @@
+<?php
+	//Get POST values
+	$subject = "Comment or Suggestion";
+	$name = $_POST["name"];
+	$lname = $_POST["lastName"];
+	$message = "Name: $name $lname
+	Date:
+	";	
+	
+	//Send Email
+	mail( "morningstarcd@tds.net", $subject,  $message, "From: morningstarcd@tds.net" );
+  
+	//Write Thank You
+	echo '<HTML><HEAD><title>Thank You</title></HEAD><BODY><font size="18">Thank you for your feedback!</font><p><a href="http://www.morningstarchild.com/">Back to HomePage</a></p></BODY></HTML>';
+?>
